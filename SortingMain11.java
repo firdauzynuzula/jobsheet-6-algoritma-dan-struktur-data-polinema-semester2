c8e1ba724c51666b1066329cd49ac10da8582b1c
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-03-31 12:49:14 +0700

    Update sorting exercises - Sorting11 and SortingMain11

--- SortingMain11.java
+++ SortingMain11.java
@@ -2,13 +2,23 @@ public class SortingMain11 {
 
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
 
         Sorting11 dataurut1 = new Sorting11(a, a.length);
+        Sorting11 dataurut2 = new Sorting11(b, b.length);
 
         System.out.println("Data awal 1 : ");
         dataurut1.tampil();
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+        System.out.println("\n");
+
+        System.out.println("Data awal 2 : ");
+        dataurut2.tampil(); 
+        dataurut2.selectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
+
     }
 }
