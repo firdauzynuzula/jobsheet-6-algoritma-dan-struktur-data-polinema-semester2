a192381d4b0373064e9882a6456df524aa81cdbc
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-04-24 20:45:58 +0700

    edit file

--- MahasiswaBerprestasi11.java
+++ MahasiswaBerprestasi11.java
@@ -20,8 +20,8 @@ public class MahasiswaBerprestasi11 {
 
     void bubbleSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
-            for (int j = 0; j < listMhs.length - 1 - i; j++) {
-                if (listMhs[j].ipk < listMhs[j - 1].ipk) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
                     // Tukar
                     Mahasiswa11 temp = listMhs[j];
                     listMhs[j] = listMhs[j - 1];
